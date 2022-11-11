da13185d5db6cee30aaf343f671a23729c094246
unknown <roanrevi@gmail.com>
2022-11-10 22:45:10 -0500

    segundo

--- codigo.js/marvel.js
+++ codigo.js/marvel.js
@@ -1,47 +1,35 @@
-
 async function peticionApi(){
 let respuesta= await fetch('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=6686afdb6f8cd0814124f7ef50466b31&hash=bd91c8200de7fae83855c96da9b4cc4b');
-
-    let data = await respuesta.json()
-
-   // console.log(data)
-
-    let hero = '';
+let data = await respuesta.json()
     for (let hero of data.data.results) {
+        
         let urlHero=hero.urls[0].url;
         
-        // console.log(hero.series.items.forEach(element => {
-        //     console.log(element)
-        // }))
-
-
+            
             document.querySelector("#contenedor").innerHTML+=`
-            <div>
-                <a href="${urlHero}" target="_blank">
+            <div>        
                     <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="">
-                    <h3>${hero.name}</h3>
-                
-                </a>
+                    
+                    <a href="${urlHero} target="_blank"><h3>${hero.name} </h3></a>
             </div>
             `
             document.querySelector("#nameavenger").innerHTML+=`                
                 <option id="nombrePersonaje">  ${hero.name} </option> <br>;                
-            `
-            let series= hero.name; 
-            
-                console.log(series) 
-        
+            `      
     }
+    let hero = data.data.results; 
+    
+    
+    console.log(hero)
     let busqueda =  document.getElementById('buscarPersonaje');
-    busqueda.addEventListener('click', function(e){
+    busqueda.addEventListener('click', ()=>{
             nuevaBusqueda =  document.querySelector("#nameavenger").value;
-            console.log(nuevaBusqueda)
-        
-        
-}) 
-            
-
+            console.log(nuevaBusqueda) 
+            function personaje() {
+                window.open('./personaje.html');
+            }
+            personaje();
+        })
 }
 
-
-peticionApi()   
+peticionApi()
