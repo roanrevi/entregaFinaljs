4ea96b640c9e29f2be4bd6152b28774b5fc1891b
unknown <roanrevi@gmail.com>
2022-11-11 22:35:35 -0500

    entrega

--- codigo.js/marvel.js
+++ codigo.js/marvel.js
@@ -1,35 +1,60 @@
+const busqueda =  document.getElementById('buscarPersonaje');
+const select =  document.querySelector("#nameavenger");
+const contenedor = document.querySelector("#contenedor");
+const modal=document.querySelector('#modal-text');
+const serie=document.querySelector('#serie');
+const dataHero=[]
+
 async function peticionApi(){
 let respuesta= await fetch('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=6686afdb6f8cd0814124f7ef50466b31&hash=bd91c8200de7fae83855c96da9b4cc4b');
 let data = await respuesta.json()
-    for (let hero of data.data.results) {
-        
-        let urlHero=hero.urls[0].url;
+let hero = data.data.results
+    //console.log(hero)
+    
+        hero.forEach(element => {
+            dataHero.push(element)
+            contenedor.innerHTML+=`
+            <div class="card" style="width: 18rem;">
+                <img src="${element.thumbnail.path}.${element.thumbnail.extension}" class="card-img-top" alt="${element.name}">
+                <div class="card-body">
+                    <p class="card-text " >${element.name}</p>
+                </div>
+            </div>`
+            select.innerHTML+=`<option  value="${element.id}"> ${element.name}
+            </option>`
+        });
         
             
-            document.querySelector("#contenedor").innerHTML+=`
-            <div>        
-                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="">
-                    
-                    <a href="${urlHero} target="_blank"><h3>${hero.name} </h3></a>
-            </div>
-            `
-            document.querySelector("#nameavenger").innerHTML+=`                
-                <option id="nombrePersonaje">  ${hero.name} </option> <br>;                
-            `      
+
+}
+//console.log(dataHero)
+const mostrarInfoPersonaje=(e)=>{
+    e.preventDefault()
+    let getValue= select.value
+    //console.log('Salida de datos', dataHero ,getValue)
+    let getHero=dataHero.filter(element=>element.id==getValue)
+    //console.log('Buscar', getHero)
+    $('#modalInfoHero').modal('toggle');
+    let series = getHero[0].series.items
+    let name=getHero[0].name
+    let urls=getHero[0].urls
+    //console.log(series)
+    series.forEach(element=>{
+        //console.log(element.name)
+        modal.innerHTML+=`
+        <p class="card-text " >${element.name}</p>`
     }
-    let hero = data.data.results; 
-    
-    
-    console.log(hero)
-    let busqueda =  document.getElementById('buscarPersonaje');
-    busqueda.addEventListener('click', ()=>{
-            nuevaBusqueda =  document.querySelector("#nameavenger").value;
-            console.log(nuevaBusqueda) 
-            function personaje() {
-                window.open('./personaje.html');
-            }
-            personaje();
-        })
+    )
+    // urls.forEach(element=>{
+    //     //console.log(element.urls)
+    //     modal.innerHTML+=`
+    //     <p class="card-text " >${element.name}</p>`
+    // }
+    // )
+
 }
 
-peticionApi()
+window.addEventListener('DOMContentLoaded',function(){
+    peticionApi()
+    select.addEventListener('change', mostrarInfoPersonaje)
+})
